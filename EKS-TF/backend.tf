6b9cd2aa26c089bbc2fbc42a430fa56793407f6a
prajith18 <105169540+prajith18@users.noreply.github.com>
2025-03-07 14:23:15 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "prajith-terraform"
+    region         = "ap-south-1"
+    key            = "global/s4/terraform.tfstate"
+    dynamodb_table = "prajith-terraform"
     encrypt        = true
   }
   required_version = ">=0.13.0"
